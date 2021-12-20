a4d0d8a2a814249084635b75f5b7a79ea92c0906
azcpavel <azc.pavel@gmail.com>
2021-12-20 15:01:39 +0600

    Menu Update

--- src/resources/views/partials/menu.blade.php
+++ src/resources/views/partials/menu.blade.php
@@ -1,35 +1,36 @@
 <?php
 $currentRouteName = request()->route()->getName();
 $menuParentEl = config('approval-config.menu-parent');
-$menuChieldEl = config('approval-config.menu-child');
-$menuChieldClass = config('approval-config.menu-child-class');
-$menuChieldActiveClass = config('approval-config.menu-child-active-class');
-$menuChieldTitlePrefix = config('approval-config.menu-child-title-prefix');
-$menuChieldTitlePostfix = config('approval-config.menu-child-title-postfix');
+$menuParentActiveClass = config('approval-config.menu-parent-active-class');
+$menuChildEl = config('approval-config.menu-child');
+$menuChildClass = config('approval-config.menu-child-class');
+$menuChildActiveClass = config('approval-config.menu-child-active-class');
 $menuLinkClass = config('approval-config.menu-link-class');
 $menuLinkActiveClass = config('approval-config.menu-link-active-class');
+$menuLinkTitlePrefix = config('approval-config.menu-link-title-prefix');
+$menuLinkTitlePostfix = config('approval-config.menu-link-title-postfix');
 $approvals = Exceptio\ApprovalPermission\Models\Approval::where('status',1)->get();
 $isMenuActive = strpos($currentRouteName,"approvals.") !== false || strpos($currentRouteName,"approval_request.") !== false;
 ?>
 
-<{{$menuChieldEl}} class="{{$menuChieldClass.' '.($isMenuActive ? $menuLinkActiveClass : '')}}">
-	<a href="javascript:void(0);" class="{{$menuLinkClass.' '.($isMenuActive ? $menuLinkActiveClass : '')}}">{!!config('approval-config.menu-parent-title')!!}</a>
+<{{$menuChildEl}} class="{{$menuChildClass.' '.($isMenuActive ? $menuParentActiveClass : '')}}">
+	<a href="javascript:void(0);" class="{{$menuLinkClass.' '.($isMenuActive ? $menuParentActiveClass : '')}}">{!!config('approval-config.menu-parent-title')!!}</a>
 
-	<{{$menuParentEl}} class="{{config('approval-config.menu-parent-class').' '.($isMenuActive ? config('approval-config.menu-parent-active-class') : '')}}">
+	<{{$menuParentEl}} class="{{config('approval-config.menu-parent-class').' '.($isMenuActive ? $menuChildActiveClass : '')}}">
 
-		<{{$menuChieldEl}} class="{{$menuChieldClass.' '.(strpos($currentRouteName,"approvals.index") !== false ? $menuChieldActiveClass : '')}}">
-		<a href="{{route('approvals.index')}}" class="{{$menuLinkClass.' '.(strpos($currentRouteName,"approvals.index") !== false ? $menuChieldActiveClass : '')}}">{!!$menuChieldTitlePrefix.'Approvals'.$menuChieldTitlePostfix!!}</a>
-		</{{$menuChieldEl}}>
+		<{{$menuChildEl}} class="{{$menuChildClass.' '.(strpos($currentRouteName,"approvals.index") !== false ? $menuLinkActiveClass : '')}}">
+		<a href="{{route('approvals.index')}}" class="{{$menuLinkClass.' '.(strpos($currentRouteName,"approvals.index") !== false ? $menuLinkActiveClass : '')}}">{!!$menuLinkTitlePrefix.'Approvals'.$menuLinkTitlePostfix!!}</a>
+		</{{$menuChildEl}}>
 
 		@foreach($approvals as $keyA => $valueA)
 			<?php
 			$isChildActive = strpos($currentRouteName,"approval_request.index") !== false && request()->route('approval')->id == $valueA->id;
 			?>
-			<{{$menuChieldEl}} class="{{$menuChieldClass.' '.($isChildActive ? $menuChieldActiveClass : '')}}">
-			<a href="{{route('approval_request.index',['approval' => $valueA->id])}}" class="{{$menuLinkClass.' '.( $isChildActive ? $menuChieldActiveClass : '')}}">{!!$menuChieldTitlePrefix.$valueA->title.$menuChieldTitlePostfix!!}</a>
-			</{{$menuChieldEl}}>
+			<{{$menuChildEl}} class="{{$menuChildClass.' '.($isChildActive ? $menuLinkActiveClass : '')}}">
+			<a href="{{route('approval_request.index',['approval' => $valueA->id])}}" class="{{$menuLinkClass.' '.( $isChildActive ? $menuLinkActiveClass : '')}}">{!!$menuLinkTitlePrefix.$valueA->title.$menuLinkTitlePostfix!!}</a>
+			</{{$menuChildEl}}>
 		@endforeach
 
 	</{{$menuParentEl}}>
 
-</{{$menuChieldEl}}>
+</{{$menuChildEl}}>
