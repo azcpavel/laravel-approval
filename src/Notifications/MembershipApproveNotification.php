cbbc181bc5ab614650b20f37727aafb05d3109a4
azcpavel <azc.pavel@gmail.com>
2024-10-21 10:11:03 +0600

    Notification Sample Update

--- src/Notifications/MembershipApproveNotification.php
+++ src/Notifications/MembershipApproveNotification.php
@@ -17,30 +17,30 @@ class MembershipApproveNotification extends Notification implements ShouldQueue
     protected $message;
     protected $subject;
     protected $approver;
-    protected $member;
+    protected $approvalItem;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct($member, $approver, $channels)
+    public function __construct($approvalRequest, $approvalItem, $approver, $channels)
     {
-        $this->member = $member;
+        $this->approvalItem = $approvalItem;
         $this->approver = $approver;
         $this->channels = $channels;
 
-        if($approver->is_approved){
-            $this->subject = 'Membership Approval Approved, Company Name: ' . $member->company_name;
+        if($approver && $approver->is_approved){
+            $this->subject = 'Membership Approval Approved, Company Name: ' . $approvalItem->company_name;
             $this->url = route('approval_request.show', $approver->approval_request_id);
-            $this->view = 'emails.membership.approval_notification';;
-            $this->message = 'Membership Approval Approved, Company Name: ' . $member->company_name;
+            $this->view = 'emails.membership.approval_notification';
+            $this->message = 'Membership Approval Approved, Company Name: ' . $approvalItem->company_name;
         }
-        elseif($approver->is_rejected) {
-            $this->subject = 'Membership Approval Rejected, Company Name: ' . $member->company_name;
+        elseif($approver && $approver->is_rejected) {
+            $this->subject = 'Membership Approval Rejected, Company Name: ' . $approvalItem->company_name;
             $this->url = route('approval_request.show', $approver->approval_request_id);
-            $this->view = 'emails.membership.approval_notification';;
-            $this->message = 'Membership Approval Rejected, Company Name: ' . $member->company_name;
+            $this->view = 'emails.membership.approval_notification';
+            $this->message = 'Membership Approval Rejected, Company Name: ' . $approvalItem->company_name;
         }
     }
 
@@ -65,7 +65,7 @@ class MembershipApproveNotification extends Notification implements ShouldQueue
     {
         return (new MailMessage)
             ->subject($this->subject)
-            ->markdown($this->view, ['member' => $this->member, 'url' => $this->url, 'user' => $notifiable, 'message' => $this->message, 'approver' => $this->approver]);
+            ->markdown($this->view, ['member' => $this->approvalItem, 'url' => $this->url, 'user' => $notifiable, 'message' => $this->message, 'approver' => $this->approver]);
     }
 
     /**
