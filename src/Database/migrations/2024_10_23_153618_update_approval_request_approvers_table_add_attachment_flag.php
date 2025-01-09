3061a72d8101d713a30500343d41697b7c3505ba
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2025-01-09 18:31:15 +0600

    Migration File Name Fix

--- src/Database/migrations/2024_10_23_153618_update_approval_request_approvers_table_add_attachment_flag.php
+++ src/Database/migrations/2024_10_23_153618_update_approval_request_approvers_table_add_attachment_flag.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateApprovalRequestApproversTableAddAttachmentFlug extends Migration
+class UpdateApprovalRequestApproversTableAddAttachmentFlag extends Migration
 {
 	/**
 	 * Run the migrations.
--- src/Database/migrations/2025_01_09_153619_update_approval_request_approvers_table_add_send_back_flag.php
+++ src/Database/migrations/2025_01_09_153619_update_approval_request_approvers_table_add_send_back_flag.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateApprovalRequestApproversTableAddSendBackFlug extends Migration
+class UpdateApprovalRequestApproversTableAddSendBackFlag extends Migration
 {
 	/**
 	 * Run the migrations.
