73dd064731b939629f48806637392950fbf9cef6
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2025-01-28 09:41:53 +0600

    Add DB index constraints to ex_approval_requests

---
+++ src/Database/migrations/2025_01_28_153619_add_index_to_ex_approval_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateApprovalRequestApproversTableAddSendBackFlag extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('ex_approval_requests', function (Blueprint $table) {
+			$table->index(['approvable_type','approvable_id']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('ex_approval_requests', function (Blueprint $table) {
+			$table->dropIndex(['approvable_type','approvable_id']);
+		});
+	}
+}
